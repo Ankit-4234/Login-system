999a0252060fd2adabd9952f6128850b9a90ddc3
Ankit Wosti <ankitwosti2@gmail.com>
2026-05-28 13:13:38 +0000

    Final commit.

---
+++ login.php
@@ -0,0 +1,30 @@
+<?php
+session_start();
+
+$conn = new mysqli("localhost", "root", "", "logindb");
+
+if ($conn->connect_error) {
+    die("<span class='error'>Connection failed</span>");
+}
+
+$email = $_POST['email'];
+$password = $_POST['password'];
+
+$sql = "SELECT * FROM users WHERE email='$email'";
+$result = $conn->query($sql);
+
+if ($result->num_rows === 1) {
+    $user = $result->fetch_assoc();
+
+    if (password_verify($password, $user['password'])) {
+        $_SESSION['user'] = $user['name'];
+        echo "success";
+    } else {
+        echo "<span class='error'>Wrong password</span>";
+    }
+} else {
+    echo "<span class='error'>Email not found</span>";
+}
+
+$conn->close();
+?>
