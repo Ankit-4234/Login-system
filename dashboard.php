87f719f9c6343ad9cb5380fdd3bf563292d60cc9
Ankit Wosti <ankitwosti2@gmail.com>
2026-05-28 13:02:41 +0000

    Dashboard commited

---
+++ dashboard.php
@@ -0,0 +1,22 @@
+<?php
+session_start();
+if(!isset($_SESSION['user'])){
+    header("location:index.html");
+    exit();
+}
+?>
+ <!DOCTYPE html>
+ <html lang="en">
+ <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Dashboard</title>
+    <link rel="stylesheet" href="style.css">
+ </head>
+ <body>
+    <div class="container">
+        <h2>Welcome, <?php echo $_SESSION['user'];?>! </h2>
+        <p> Logged in successfully. </p>
+</div>
+ </body>
+ </html>
